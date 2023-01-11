2f7acef02cdf217447b44d9bc0a9dbcd68cc2954
andrewrgarcia <andrewrgarcia@ufl.edu>
2023-01-11 15:34:05 -0500

    add py files in extra for stand-alone usage examples

---
+++ extra/land.py
@@ -0,0 +1,11 @@
+#land.py
+import voxelmap as vxm
+import numpy as np
+
+img = vxm.Image('Desktop/land.png')       # incorporate fake land topography .png file
+
+img.make(1)                             # resized to 1.0x original size i.e. not resized (default)
+
+img.ImageMesh('land.obj',  12, 0.52, 1, False, figsize=(10,10))
+
+img.MeshView(viewport=(1152, 1152))
