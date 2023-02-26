38e1c3dbdbf075d37e7668d940b98d0d2f39fb86
andrewrgarcia <garcia.gtr@gmail.com>
2023-02-26 14:09:31 -0500

    add skull model script

---
+++ extra/skull.py
@@ -0,0 +1,17 @@
+import voxelmap as vxm
+
+model = vxm.Model()
+
+model.load('./skull.txt')
+
+arr = model.array 
+
+
+model.array = model.array[::-1]
+
+'draw in standard voxel form'
+model.draw('voxels',wireframe=True, background_color='#3e404e',window_size=[700,700])
+
+'to convert to mesh'
+model.MarchingMesh()
+model.MeshView(wireframe=True,alpha=1,color=True,background_color='#b064fd',viewport=[700,700])
